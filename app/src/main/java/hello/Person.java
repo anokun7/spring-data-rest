79952f133aabb14e4e40803ac70e96c99fea8627
Anoop Kumar <anoop@Anoops-MacBook-Pro-2.local>
2017-09-18 16:40:49 -0400

    Added email capture

--- app/src/main/java/hello/Person.java
+++ app/src/main/java/hello/Person.java
@@ -14,6 +14,7 @@ public class Person {
 
     private String firstName;
     private String lastName;
+    private String email;
 
     public String getFirstName() {
         return firstName;
@@ -29,5 +30,13 @@ public class Person {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
